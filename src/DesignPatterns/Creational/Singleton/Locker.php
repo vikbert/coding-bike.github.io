dd8635bc44b500fba3470e63d1e629318f170fd0
vikbert <segentor@gmail.com>
2018-08-12 13:11:09 +0200

    abstract factory

--- src/DesignPatterns/Creational/Singleton/Locker.php
+++ src/DesignPatterns/Creational/Singleton/Locker.php
@@ -1,1 +1,50 @@
-2
+<?php
+
+declare(strict_types = 1);
+
+namespace DesignPatterns\Creational\Singleton;
+
+final class Locker
+{
+    /** @var Locker */
+    private static $instance;
+
+    /** @var bool */
+    private $isLocked = false;
+
+    public static function getInstance(): Locker
+    {
+        if (null === static::$instance) {
+            static::$instance = new Static();
+        }
+
+        return self::$instance;
+    }
+
+    public function lock()
+    {
+        $this->isLocked = true;
+    }
+
+    public function unlock()
+    {
+        $this->isLocked = false;
+    }
+
+    public function isLocked(): bool
+    {
+        return $this->isLocked;
+    }
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+}
